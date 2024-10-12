71fe02f3725b56fca4b93b1cb19e3d2a4fb973e2
wook <yho7955@naver.com>
2024-10-12 11:42:17 +0900

    refactor: 불필요한 코드 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,12 @@
-'use client';
-
 import React from 'react';
 import AuthHandler from '@/components/AuthHandler';
 import HomeMain from '@/components/Home/HomeMain';
 import HeaderNav from '@/components/Header/HeaderNav';
-import Dropbox from '@/lib/common/Dropbox';
 export default function HomePage() {
   return (
     <>
       <HeaderNav />
       <HomeMain />
-      <Dropbox
-        options={[
-          { value: 'option1', label: '옵션 1' },
-          { value: 'option2', label: '옵션 2' },
-          { value: 'option3', label: '옵션 3' },
-        ]}
-        placeholder="선택하세요"
-        className="w-[18rem]"
-      />
       <AuthHandler />
     </>
   );
